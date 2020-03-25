f8f5c339f9afc9d192dd4670f4b776f713e0a5f8
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-25 22:33:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -83,7 +83,7 @@ def movies_with_directors_set(source)
   newarray2=[]
   element_index=0 
   while element_index<source.length do
-  newarray2 << [source[element_index][:movies]]
+  newarray2 << [{:title => "banana", :director_name => source[element_index][:name], :worldwide_gross => source[element_index][:movies]}]
   element_index+=1 
 end
   newarray2
