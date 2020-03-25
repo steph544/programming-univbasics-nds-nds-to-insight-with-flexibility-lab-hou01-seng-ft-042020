e5907aaa0e3236fbe77195e74764b2c11951d9a4
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-25 20:36:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,7 @@ end
   studio={}
   counter=0 
   while counter<collection.length do
-    movie_info= collection[counter][:movie]
+    movie_info= collection[counter]
     binding.pry 
     if !studio[movie_info[:studio]] 
       studio[movie_info[:studio]]=movie_info[:worldwide_gross] 
